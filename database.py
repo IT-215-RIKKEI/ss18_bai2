2bbf525dd4a4640d6d218f105342a7acb03440ce
Văn Phúc Sang <vanphucsang2@gmail.com>
2026-07-16 12:18:42 +0700

    Update database Python script

--- database.py
+++ database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./company.db"
-
+DATABASE_URL = "sqlite:///./classroom.db"
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
-
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
